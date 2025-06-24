0fed1acb62a11a91ca23780ce21619e5a3d2eed2
Furrociuos <marcbenj2006@gmail.com>
2025-06-24 15:58:58 +0800

    Changed stuff

--- .config/nwg-wrapper/date.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-date "+%B %d, %Y"
--- Scripts/Date.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sleep 5s
-nwg-wrapper -s date.sh -c date.css -r 180000 \
-  -j center -a start -p left \
-  -ml 323 -mt 235 &
-nwg-wrapper -s day.sh -c day.css -r 180000 \
-  -j center -a start -p left \
-  -ml 213 -mt 125
